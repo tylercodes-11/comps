f7a73151eed09043b1149138abec35e3e8bb8c9f
Ty Reid <trawx11@gmail.com>
2023-02-20 16:10:20 -0500

    panel styling component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
-
+import { GoChevronDown } from 'react-icons/go';
 import { useState } from 'react';
-
+import Panel from './Panel';
 
 
 function Dropdown({ options, value, onChange }) {
@@ -18,7 +18,7 @@ function Dropdown({ options, value, onChange }) {
     };
 
     const renderedOptions = options.map((option) => {
-        return ( <div onClick={() => handleOptionClick(option)} key={option.value}>
+        return ( <div className='hover:bg-sky-100 rounded cursor-pointer p-1' onClick={() => handleOptionClick(option)} key={option.value}>
             {option.label}
         </div>
         );
@@ -29,11 +29,15 @@ function Dropdown({ options, value, onChange }) {
     return (
 
            
-    <div>
-        <div onClick={handleClick}>{value?.label || 'Select...'}
-            </div>
+    
+        <div className='w-48 relative'>
+            <Panel className='flex justify-between items-center cursor-pointer ' onClick={handleClick}>{value?.label || 'Select...'}
+            <GoChevronDown />
+            </Panel>
             
-        {isOpen && <div>{renderedOptions}</div>}
+        {isOpen && (
+        <Panel className='absolute top-full '>{renderedOptions} </Panel >
+        )}
  </div>
     );
 }
