ea01b460b5a803f6bd8260f06a55b9eacd96a75b
Ty Reid <trawx11@gmail.com>
2023-02-22 21:04:32 -0500

    pages no refesh, meta keys ctrl key on links

---
+++ src/components/Link.js
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import NavigationContext from '../context/navigation';
+
+function Link({ to, children }) {
+    const { navigate } = useContext(NavigationContext);
+    const handleClick = (event) => {
+        if (event.metaKey || event.ctrl.Key) {
+            return;
+        }
+        console.log(event);
+        event.preventDefault();
+        navigate (to)
+    };
+    return <a href={to} onClick={handleClick}>{children}</a>
+};
+
+export default Link;
