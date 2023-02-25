c50456412815ae7abb2e64448370e2b5b71e34ef
Ty Reid <trawx11@gmail.com>
2023-02-25 10:51:22 -0500

    i accept close modal click added

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom';
 
-function Modal({ onClose }) {
+function Modal({ onClose, children, actionBar }) {
     return ReactDOM.createPortal(
     <div>
         <div onClick={onClose} 
         className="absolute inset-0 bg-gray-300 opacity-80"></div>
-        <div className="absolute inset-40 p-10 bg-white"> Im a Modal!
+        <div className="absolute inset-40 p-10 bg-white"> 
+        {children}
+        {actionBar}
+
 
         </div>
     </div>,
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -14,12 +14,21 @@ const handleClose = () => {
     setShowModal(false);
 };
 
+const actionBar = <Button primary onClick={handleClose} >I Accept!</Button>
+
+const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+<p>
+Here is an important agreement for you to accept
+</p>
+</Modal>
+
     return (
          <div>
         <Button onClick={handleClick} primary>
             Open Modal
         </Button>
-        {showModal && <Modal onClose={handleClose}/>}
+    {showModal && modal}
+
     </div>
     );
 }
